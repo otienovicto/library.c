8f36403030e0ca6ad210e333705849a4c1ea9cf5
otienovicto <90698830+otienovicto@users.noreply.github.com>
2021-10-12 10:36:37 +0300

    Add files via upload

---
+++ Librarysystem.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tLIBRARY SYSTEM\n");
+    printf("\tWELCOME TO THE LIBRARY\n");
+    printf("What would you like to do in the Library?\n");
+    printf("1) Borrow books\n");
+    printf("2) Read in the Library\n");
+    printf("3) Visit E-library\n");
+    printf("4) Visit consoltancy\n");
+    printf("5) Explore Library\n");
+    printf("Press any key to exit\n");
+
+    return 0;
+}
